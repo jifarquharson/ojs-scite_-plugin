17bdcdd21eed5aae96c92a126d187212bc74ef01
Jamie Farquharson <jifarq89@googlemail.com>
2023-03-09 11:30:06 +0100

    Create sciteAnalyticsPlugin.php

---
+++ sciteAnalyticsPlugin.php
@@ -0,0 +1,35 @@
+<?php
+import('lib.pkp.classes.plugins.GenericPlugin');
+class sciteAnalyticsPlugin extends GenericPlugin {
+	public function register($category, $path, $mainContextId = NULL) {
+
+    // Register the plugin even when it is not enabled
+    $success = parent::register($category, $path);
+
+		if ($success && $this->getEnabled()) {
+      // Do something when the plugin is enabled
+    }
+
+		return $success;
+	}
+
+  /**
+   * Provide a name for this plugin
+   *
+   * The name will appear in the plugins list where editors can
+   * enable and disable plugins.
+   */
+	public function getDisplayName() {
+		return 'scite_ Analytics Badge';
+	}
+
+	/**
+   * Provide a description for this plugin
+   *
+   * The description will appear in the plugins list where editors can
+   * enable and disable plugins.
+   */
+	public function getDescription() {
+		return 'This plugin is an example created for a tutorial on how to create a plugin.';
+	}
+}
