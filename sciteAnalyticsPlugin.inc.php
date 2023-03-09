66595b7adbcc0b111d087a9642c5d1b3d52aac7f
Jamie Farquharson <jifarq89@googlemail.com>
2023-03-09 12:06:39 +0100

    Update and rename sciteAnalyticsPlugin.php to sciteAnalyticsPlugin.inc.php

--- sciteAnalyticsPlugin.inc.php
+++ sciteAnalyticsPlugin.inc.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * @file plugins/generic/sciteAnalytics/sciteAnalyticsPlugin.inc.php
+ * 
+ * Copyright 2023
+ *
+ * @author jifarquharson
+ *
+ */
 import('lib.pkp.classes.plugins.GenericPlugin');
 class sciteAnalyticsPlugin extends GenericPlugin {
+	/**
+	 * Called as a plugin is registered to the registry
+	 * @param $category String Name of category plugin was registered to
+	 * @return boolean True iff plugin initialized successfully; if false,
+	 * 	the plugin will not be registered.
+	 */
 	public function register($category, $path, $mainContextId = NULL) {
 
     // Register the plugin even when it is not enabled
@@ -19,9 +33,12 @@ class sciteAnalyticsPlugin extends GenericPlugin {
    * The name will appear in the plugins list where editors can
    * enable and disable plugins.
    */
-	public function getDisplayName() {
-		return 'scite_ Analytics Badge';
+	function getDisplayName() {
+		return __('plugins.generic.sciteAnalytics.displayName');
 	}
+	/*public function getDisplayName() {
+		return 'scite_ Analytics Badge';
+	}/*
 
 	/**
    * Provide a description for this plugin
@@ -32,4 +49,13 @@ class sciteAnalyticsPlugin extends GenericPlugin {
 	public function getDescription() {
 		return 'This plugin integates a [scite.ai](https://scite.ai/badge) badge to give a breakdown of article citations.';
 	}
+	private function getPubId($smarty) {
+		$application = Application::getName();
+		return $submission->getStoredPubId('doi');
+	}
 }
+	/**
+	 * Add badge to article landing page
+	 * @param $hookName string
+	 * @param $params array
+	 */
