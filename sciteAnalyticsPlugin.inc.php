7bcf34245cce6cb9a8d6aa71654a81db3b3e1236
Jamie Farquharson <jifarq89@googlemail.com>
2023-03-09 12:11:58 +0100

    Update sciteAnalyticsPlugin.inc.php

--- sciteAnalyticsPlugin.inc.php
+++ sciteAnalyticsPlugin.inc.php
@@ -16,6 +16,14 @@ class sciteAnalyticsPlugin extends GenericPlugin {
 	 * 	the plugin will not be registered.
 	 */
 	public function register($category, $path, $mainContextId = NULL) {
+		$success = parent::register($category, $path);
+		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return $success;
+		if ($success && $this->getEnabled()) {
+			HookRegistry::register('Templates::Article::Details', array($this, 'insertBadge'));
+		}
+		return $success;
+  	}
+	/*public function register($category, $path, $mainContextId = NULL) {
 
     // Register the plugin even when it is not enabled
     $success = parent::register($category, $path);
@@ -25,7 +33,7 @@ class sciteAnalyticsPlugin extends GenericPlugin {
     }
 
 		return $success;
-	}
+	}*/
 
   /**
    * Provide a name for this plugin
@@ -49,13 +57,32 @@ class sciteAnalyticsPlugin extends GenericPlugin {
 	public function getDescription() {
 		return 'This plugin integates a [scite.ai](https://scite.ai/badge) badge to give a breakdown of article citations.';
 	}
-	private function getPubId($smarty) {
-		$application = Application::getName();
-		return $submission->getStoredPubId('doi');
-	}
-}
+	
 	/**
-	 * Add badge to article landing page
-	 * @param $hookName string
-	 * @param $params array
+	 * Insert Connected Papers badge
+	 * 
+	 * @param string $hookName Name of hook calling function
+	 * @param array $params 
+	 * @return boolean
 	 */
+	function insertBadge($hookName, $params) {
+
+		$templateMgr = TemplateManager::getManager();
+
+		// get DOI
+		$submission = $templateMgr->getTemplateVars('article');
+		$doi = $submission->getStoredPubId('doi');
+		$templateMgr->assign('DOI', $doi);
+
+		// get scite_ badge template
+		$badge = $this->getTemplateResource('sciteBadge.tpl');
+
+		if ($doi) {
+			$output =& $params[2]; 
+			$output .= $templateMgr->fetch($badge);
+		}
+
+		return false;
+	}
+}	
+?>
