ed5ed19ec8029640970281ee9d956c271596c26a
Jamie Farquharson <jifarq89@googlemail.com>
2023-03-09 12:20:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
+
+/**
+ * @defgroup plugins_generic_sciteAnalytics
+ */
+ 
+/**
+ * @file plugins/generic/sciteAnalytics/index.php
+ *
+ *
+ * @ingroup plugins_generic_sciteAnalytics
+ * @brief Wrapper for scite Section Badge plugin
+ *
+ */
+
 require_once('sciteAnalyticsPlugin.php');
 return new sciteAnalyticsPlugin();
+
+?>
